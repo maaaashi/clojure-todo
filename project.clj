d830d3cdeca474fa81ab763e1bcc29ad3f43477b
maaaashi <doyaseizin.7@gmail.com>
2024-12-09 22:00:43 +0900

    ring init

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [nubank/mockfn "0.7.0"]]
+                 [nubank/mockfn "0.7.0"]
+                 [ring/ring-core "1.13.0"]
+                 [ring/ring-jetty-adapter "1.13.0"]]
+  :main todo.core
   :repl-options {:init-ns todo.core})
--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,5 +1,6 @@
 (ns todo.core
-  (:require [todo.http :as http]))
+  (:require [todo.http :as http]
+            [ring.adapter.jetty :as jetty]))
 
 (defn get-todo-titles
   []
@@ -12,3 +13,19 @@
     (:title body)))
 
 (get-todo-titles)
+
+(defn handler
+  [request]
+  (println request)
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, World!"})
+
+(defn -main []
+  (println "アプリケーションを起動します")
+  (jetty/run-jetty handler {:port 3000}))
+
+(defn logging-middleware [handler]
+  (fn [request]
+    (println "Request received:" request)
+    (handler request)))
